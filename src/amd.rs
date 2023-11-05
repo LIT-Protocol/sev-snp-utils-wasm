dc8ad24948138c8dddfe045ec0610d516a7e5681
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2023-11-04 20:31:22 -0400

    it almost works.  stupid CORS breaking the internet.

---
+++ src/amd.rs
@@ -0,0 +1,111 @@
+use js_sys::ArrayBuffer;
+use sev::certs::snp::Certificate;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::console;
+use web_sys::{Blob, Request, RequestInit, RequestMode, Response};
+
+const KDS_CERT_SITE: &str = "https://kdsintf.amd.com";
+#[allow(dead_code)]
+const KDS_DEV_CERT_SITE: &str = "https://kdsintfdev.amd.com";
+#[allow(dead_code)]
+const KDS_CEK: &str = "/cek/id/";
+const KDS_VCEK: &str = "/vcek/v1/"; // KDS_VCEK/{product_name}/{hwid}?{tcb parameter list}
+#[allow(dead_code)]
+const KDS_VCEK_CERT_CHAIN: &str = "cert_chain"; // KDS_VCEK/{product_name}/cert_chain
+#[allow(dead_code)]
+const KDS_VCEK_CRL: &str = "crl"; // KDS_VCEK/{product_name}/crl"
+
+const ASK_DER_FILENAME: &str = "ask.der";
+const ASK_PEM_FILENAME: &str = "ask.pem";
+const ARK_DER_FILENAME: &str = "ark.der";
+const ARK_PEM_FILENAME: &str = "ark.pem";
+
+const ARK_FETCH_LOCK_FILE: &str = "ark_fetch.lock";
+
+pub fn get_kds_vcek_cert_chain_url(product_name: &str) -> String {
+    format!(
+        "{}{}{}/{}",
+        KDS_CERT_SITE, KDS_VCEK, product_name, KDS_VCEK_CERT_CHAIN
+    )
+}
+
+// pub async fn fetch_kds_vcek_cert_chain_pem(product_name: &str) -> Result<Certificate, JsValue> {
+//     let url = get_kds_vcek_cert_chain_url(product_name);
+
+//     let cert_blob = fetch_blob(url)
+//         .await
+//         .expect("Could not get cert chain from KDS");
+
+//     Certificate::from_pem(&blob_into_bytes(cert_blob).await).map_err(|e| e.to_string().into())
+// }
+
+pub fn get_kds_vcek_der_url(
+    product_name: &str,
+    chip_id: &str,
+    boot_loader: u8,
+    tee: u8,
+    snp: u8,
+    microcode: u8,
+) -> String {
+    format!(
+        "{}{}{}/{}?blSPL={:0>2}&teeSPL={:0>2}&snpSPL={:0>2}&ucodeSPL={:0>2}",
+        KDS_CERT_SITE, KDS_VCEK, product_name, chip_id, boot_loader, tee, snp, microcode
+    )
+}
+
+pub async fn fetch_kds_vcek_der(
+    product_name: &str,
+    chip_id: &str,
+    boot_loader: u8,
+    tee: u8,
+    snp: u8,
+    microcode: u8,
+) -> Result<Certificate, JsValue> {
+    let url = get_kds_vcek_der_url(product_name, chip_id, boot_loader, tee, snp, microcode);
+
+    let cert_array_buffer = fetch_blob(url)
+        .await
+        .expect("Could not get cert chain from KDS");
+
+    let cert_vec = js_sys::Uint8Array::new(&cert_array_buffer).to_vec();
+    Certificate::from_pem(&cert_vec).map_err(|e| e.to_string().into())
+}
+
+pub async fn fetch_blob(url: String) -> Result<ArrayBuffer, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::NoCors);
+
+    let request = Request::new_with_str_and_init(&url, &opts)?;
+
+    // request
+    //     .headers()
+    //     .set("Accept", "application/vnd.github.v3+json")?;
+
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+    // `resp_value` is a `Response` object.
+    // assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    // Convert this other `Promise` into a rust `Future`.
+    let resolved_array_buffer = JsFuture::from(resp.array_buffer()?).await?;
+    let resolved_array_buffer: ArrayBuffer = resolved_array_buffer.dyn_into().unwrap();
+    console::log_1(&resolved_array_buffer.byte_length().into());
+    // let blob = Blob::new_with_blob_sequence(&resolved_blob).expect("Could not convert to blob");
+
+    // Send the JSON response back to JS.
+    Ok(resolved_array_buffer)
+}
+
+async fn blob_into_bytes(blob: Blob) -> Vec<u8> {
+    let array_buffer_promise: JsFuture = blob.array_buffer().into();
+
+    let array_buffer: JsValue = array_buffer_promise
+        .await
+        .expect("Could not get ArrayBuffer from file");
+
+    js_sys::Uint8Array::new(&array_buffer).to_vec()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,58 @@
+mod amd;
 mod utils;
 
 use wasm_bindgen::prelude::*;
+
+use sev::certs::snp::{builtin::milan, ca, Certificate, Chain, Verifiable};
+use sev::firmware::guest::AttestationReport;
+use web_sys::console;
 
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
 }
 
+pub(crate) fn fmt_bin_vec_to_hex(vec: &Vec<u8>) -> String {
+    vec.iter().map(|b| format!("{:02x}", b)).collect::<String>()
+}
+
 #[wasm_bindgen]
-pub fn greet() {
+pub async fn verify_attestation_report(attestation_report: JsValue) -> Result<(), JsValue> {
+    console::log_1(&"Checking attestation report...".into());
+    console_error_panic_hook::set_once();
+
+    let uint8_report = js_sys::Uint8Array::new(&attestation_report);
+    let report_bytes = uint8_report.to_vec();
+
+    let report: AttestationReport = unsafe { std::ptr::read(report_bytes.as_ptr() as *const _) };
+
+    // let uint8arrayReport = js_sys::Uint8Array::new(&attestation_report);
+    // console::log_1(&"Bit 0:".into());
+    // console::log_1(&uint8arrayReport.at(0).into());
+    // let report: AttestationReport = serde_wasm_bindgen::from_value(attestation_report)
+    //     .expect("Could not parse attestation report");
+
+    console::log_1(&"Got report".into());
+
+    let ark = milan::ark().unwrap();
+    let ask = milan::ask().unwrap();
+    // let vcek = Certificate::from_der(TEST_MILAN_VCEK_DER).unwrap();
+    let vcek = amd::fetch_kds_vcek_der(
+        "Milan",
+        &fmt_bin_vec_to_hex(&report.chip_id.to_vec()),
+        report.reported_tcb.bootloader,
+        report.reported_tcb.tee,
+        report.reported_tcb.snp,
+        report.reported_tcb.microcode,
+    )
+    .await
+    .expect("Could not get vcek");
+
+    let ca = ca::Chain { ark, ask };
+
+    let chain = Chain { ca, vcek };
+
+    let _ = (&chain, &report).verify();
     alert("Hello, sev-snp-utils-wasm!");
+    Ok(())
 }
