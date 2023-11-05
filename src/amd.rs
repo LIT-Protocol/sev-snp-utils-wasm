253e3dc43b2cdf3d19df565fb4ba74183f5be25d
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2023-11-05 15:44:23 -0500

    neat, it works

--- src/amd.rs
+++ src/amd.rs
@@ -1,10 +1,9 @@
-use crate::utils::fmt_bin_vec_to_hex;
+use base64_light::{base64_decode, base64_encode_bytes};
 use js_sys::ArrayBuffer;
 use sev::certs::snp::Certificate;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::JsFuture;
-use web_sys::console;
-use web_sys::{Blob, Request, RequestInit, RequestMode, Response, Storage};
+use web_sys::{Request, RequestInit, RequestMode, Response};
 
 const KDS_CERT_SITE: &str = "https://kdsintf.amd.com";
 #[allow(dead_code)]
@@ -16,30 +15,6 @@ const KDS_VCEK: &str = "/vcek/v1/"; // KDS_VCEK/{product_name}/{hwid}?{tcb param
 const KDS_VCEK_CERT_CHAIN: &str = "cert_chain"; // KDS_VCEK/{product_name}/cert_chain
 #[allow(dead_code)]
 const KDS_VCEK_CRL: &str = "crl"; // KDS_VCEK/{product_name}/crl"
-
-const ASK_DER_FILENAME: &str = "ask.der";
-const ASK_PEM_FILENAME: &str = "ask.pem";
-const ARK_DER_FILENAME: &str = "ark.der";
-const ARK_PEM_FILENAME: &str = "ark.pem";
-
-const ARK_FETCH_LOCK_FILE: &str = "ark_fetch.lock";
-
-pub fn get_kds_vcek_cert_chain_url(product_name: &str) -> String {
-    format!(
-        "{}{}{}/{}",
-        KDS_CERT_SITE, KDS_VCEK, product_name, KDS_VCEK_CERT_CHAIN
-    )
-}
-
-// pub async fn fetch_kds_vcek_cert_chain_pem(product_name: &str) -> Result<Certificate, JsValue> {
-//     let url = get_kds_vcek_cert_chain_url(product_name);
-
-//     let cert_blob = fetch_blob(url)
-//         .await
-//         .expect("Could not get cert chain from KDS");
-
-//     Certificate::from_pem(&blob_into_bytes(cert_blob).await).map_err(|e| e.to_string().into())
-// }
 
 pub fn get_kds_vcek_der_url(
     product_name: &str,
@@ -71,23 +46,23 @@ pub async fn fetch_kds_vcek_der(
         .local_storage()?
         .expect("Could not get local storage");
     if let Ok(Some(value)) = local_storage.get_item(&url) {
-        let decoded = hex::decode(value).expect("Could not decode cert chain from cache");
+        let decoded = base64_decode(&value);
         return Certificate::from_der(&decoded).map_err(|e| e.to_string().into());
     }
 
-    let cert_array_buffer = fetch_blob(url.clone())
+    let cert_array_buffer = fetch_array_buffer(url.clone())
         .await
         .expect("Could not get cert chain from KDS");
 
     let cert_vec = js_sys::Uint8Array::new(&cert_array_buffer).to_vec();
 
     // store in cache
-    local_storage.set_item(&url, &hex::encode(&cert_vec))?;
+    local_storage.set_item(&url, &base64_encode_bytes(&cert_vec))?;
 
     Certificate::from_der(&cert_vec).map_err(|e| e.to_string().into())
 }
 
-pub async fn fetch_blob(url: String) -> Result<ArrayBuffer, JsValue> {
+pub async fn fetch_array_buffer(url: String) -> Result<ArrayBuffer, JsValue> {
     let mut opts = RequestInit::new();
     opts.method("GET");
     opts.mode(RequestMode::Cors);
@@ -97,10 +72,6 @@ pub async fn fetch_blob(url: String) -> Result<ArrayBuffer, JsValue> {
 
     let request = Request::new_with_str_and_init(&url, &opts)?;
 
-    // request
-    //     .headers()
-    //     .set("Accept", "application/vnd.github.v3+json")?;
-
     let window = web_sys::window().unwrap();
     let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
 
@@ -108,22 +79,16 @@ pub async fn fetch_blob(url: String) -> Result<ArrayBuffer, JsValue> {
     // assert!(resp_value.is_instance_of::<Response>());
     let resp: Response = resp_value.dyn_into().unwrap();
 
+    if !resp.ok() {
+        return Err(JsValue::from_str(&format!(
+            "fetch_array_buffer failed to get the vcek from AMD servers with status code: {}",
+            resp.status()
+        )));
+    }
+
     // Convert this other `Promise` into a rust `Future`.
-    let resolved_array_buffer = JsFuture::from(resp.array_buffer()?).await?;
-    let resolved_array_buffer: ArrayBuffer = resolved_array_buffer.dyn_into().unwrap();
-    console::log_1(&resolved_array_buffer.byte_length().into());
-    // let blob = Blob::new_with_blob_sequence(&resolved_blob).expect("Could not convert to blob");
+    let resolved_js_value = JsFuture::from(resp.array_buffer()?).await?;
+    let resolved_array_buffer: ArrayBuffer = resolved_js_value.dyn_into().unwrap();
 
-    // Send the JSON response back to JS.
     Ok(resolved_array_buffer)
-}
-
-async fn blob_into_bytes(blob: Blob) -> Vec<u8> {
-    let array_buffer_promise: JsFuture = blob.array_buffer().into();
-
-    let array_buffer: JsValue = array_buffer_promise
-        .await
-        .expect("Could not get ArrayBuffer from file");
-
-    js_sys::Uint8Array::new(&array_buffer).to_vec()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod amd;
 mod utils;
 
+use base64_light::base64_decode;
 use wasm_bindgen::prelude::*;
 
 use sev::certs::snp::{builtin::milan, ca, Chain, Verifiable};
@@ -8,32 +9,50 @@ use sev::firmware::guest::AttestationReport;
 use web_sys::console;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+#[doc = "Parses and returns the parsed attestation report"]
+pub fn parse_attestation_report(attestation_report: &str) -> Result<JsValue, JsValue> {
+    console::log_1(&"Parsing attestation report...".into());
+    utils::set_panic_hook();
+
+    let report_bytes = base64_decode(attestation_report);
+    let report: AttestationReport = unsafe { std::ptr::read(report_bytes.as_ptr() as *const _) };
+    return Ok(serde_wasm_bindgen::to_value(&report).expect("Could not convert report to JsValue"));
 }
 
 #[wasm_bindgen]
-pub async fn verify_attestation_report(attestation_report: JsValue) -> Result<(), JsValue> {
-    console::log_1(&"Checking attestation report...".into());
-
+#[doc = "Gets the vcek url for the given attestation report.  You can fetch this certificate yourself, and if you put it into LocalStorage with the url as the key and the response body as base64 encoded value, then the next time you call verify_attestation_report it will use the cached value instead of fetching it again."]
+pub fn get_vcek_url(attestation_report: &str) -> Result<JsValue, JsValue> {
+    // console::log_1(&"Getting vcek url...".into());
     utils::set_panic_hook();
 
-    let uint8_report = js_sys::Uint8Array::new(&attestation_report);
-    let report_bytes = uint8_report.to_vec();
-
+    let report_bytes = base64_decode(attestation_report);
     let report: AttestationReport = unsafe { std::ptr::read(report_bytes.as_ptr() as *const _) };
 
-    // let uint8arrayReport = js_sys::Uint8Array::new(&attestation_report);
-    // console::log_1(&"Bit 0:".into());
-    // console::log_1(&uint8arrayReport.at(0).into());
-    // let report: AttestationReport = serde_wasm_bindgen::from_value(attestation_report)
-    //     .expect("Could not parse attestation report");
+    let url = amd::get_kds_vcek_der_url(
+        "Milan",
+        &utils::fmt_bin_vec_to_hex(&report.chip_id.to_vec()),
+        report.reported_tcb.bootloader,
+        report.reported_tcb.tee,
+        report.reported_tcb.snp,
+        report.reported_tcb.microcode,
+    );
 
-    console::log_1(&"Got report".into());
+    return Ok(JsValue::from_str(&url));
+}
+
+#[wasm_bindgen]
+pub async fn verify_attestation_report(attestation_report: &str) -> Result<(), JsValue> {
+    // console::log_1(&"Checking attestation report...".into());
+    utils::set_panic_hook();
+
+    let report_bytes = base64_decode(attestation_report);
+    let report: AttestationReport = unsafe { std::ptr::read(report_bytes.as_ptr() as *const _) };
+
+    // console::log_1(&"Got report: ".into());
+    // console::log_1(&report.to_string().into());
 
     let ark = milan::ark().unwrap();
     let ask = milan::ask().unwrap();
-    // let vcek = Certificate::from_der(TEST_MILAN_VCEK_DER).unwrap();
     let vcek = amd::fetch_kds_vcek_der(
         "Milan",
         &utils::fmt_bin_vec_to_hex(&report.chip_id.to_vec()),
@@ -55,6 +74,5 @@ pub async fn verify_attestation_report(attestation_report: JsValue) -> Result<()
         console::log_1(&e.to_string().into());
         return Err(e.to_string().into());
     }
-    alert("Hello, sev-snp-utils-wasm!");
     Ok(())
 }
